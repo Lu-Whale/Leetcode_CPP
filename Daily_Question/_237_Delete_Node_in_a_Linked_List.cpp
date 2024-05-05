d3f1e18860adb86f7a0c1f051d33326ffbacc085
Lucas Lu <lujingyu2919@gmail.com>
2024-05-05 14:24:00 +1000

    5.5

---
+++ Daily_Question/_237_Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* next = node->next;
+        node->val = next->val;
+        node->next = next->next;
+    }
+};
