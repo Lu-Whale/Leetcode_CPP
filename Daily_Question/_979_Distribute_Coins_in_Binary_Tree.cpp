e4e22dc8e2b6b0bedd7efbcaddc9f5a5d3743bb6
Lucas Lu <lujingyu2919@gmail.com>
2024-05-19 00:53:12 +1000

    5.18

--- Daily_Question/_979_Distribute_Coins_in_Binary_Tree.cpp
+++ Daily_Question/_979_Distribute_Coins_in_Binary_Tree.cpp
@@ -2,6 +2,7 @@
 // Created by 卢京宇 on 18/5/2024.
 //
 #include <vector>
+#include <cmath>
 using namespace std;
 
 struct TreeNode {
@@ -19,8 +20,15 @@ int ans = 0;
     pair<int, int> dfs(TreeNode *node) {
         if (node == nullptr)
             return {0, 0};
-        auto [coins_l, nodes_l] = dfs(node->left);
-        auto [coins_r, nodes_r] = dfs(node->right);
+         // 递归处理左子树和右子树
+        auto coins_l_nodes_l = dfs(node->left); // 使用一个临时变量来接收返回值
+        int coins_l = coins_l_nodes_l.first;
+        int nodes_l = coins_l_nodes_l.second;
+
+        auto coins_r_nodes_r = dfs(node->right); // 使用一个临时变量来接收返回值
+        int coins_r = coins_r_nodes_r.first;
+        int nodes_r = coins_r_nodes_r.second;
+
         int coins = coins_l + coins_r + node->val; // 子树硬币个数
         int nodes = nodes_l + nodes_r + 1; // 子树节点数
         ans += abs(coins - nodes);
